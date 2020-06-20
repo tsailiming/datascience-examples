42a2873bf648f461af21c2726f50c25c4fe6e33f
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-20 21:31:21 +0800

    update

--- workshop/src/train/train.sh
+++ workshop/src/train/train.sh
@@ -3,4 +3,5 @@
 pip install -U pip setuptools
 pip install -r /workspace/source/workshop/src/train/requirements.txt
 
+echo "Running training"
 python /workspace/source/workshop/src/train/lr.py -m /workspace/model 
