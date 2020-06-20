91fee2df432ffecd80b517dbbf12a9f8736bb8d7
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-20 22:07:50 +0800

    Add run name

--- workshop/src/train/train.sh
+++ workshop/src/train/train.sh
@@ -4,4 +4,4 @@ pip install -U pip setuptools
 pip install -r /workspace/source/workshop/src/train/requirements.txt
 
 echo "Running training"
-python /workspace/source/workshop/src/train/lr.py -m /workspace/model 
+python /workspace/source/workshop/src/train/lr.py -m /workspace/model -r lr
