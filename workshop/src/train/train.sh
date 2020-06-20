54237c8fd3d407c69f251647b6f65e379fe32dcc
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-20 16:35:07 +0800

    Update requirements

--- workshop/src/train/train.sh
+++ workshop/src/train/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -U pip
+pip install -U pip setuptools
 pip install -r /workspace/source/workshop/src/train/requirements.txt
 
 python /workspace/source/workshop/src/train/lr.py -m /workspace/model 
