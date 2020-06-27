596206114e10e669a3d8f14c7f5436cf54622661
Tsai Li Ming <tsai.liming@gmail.com>
2020-06-27 11:53:42 +0800

    update

--- workshop/src/train/train-dev.sh
+++ workshop/src/train/train-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pip install -U pip setuptools grpcio-tools==1.29.0 seldon-core==1.1.0 gitdb==4.0.5 protobuf==3.12.2
+pip install -U pip setuptools grpcio-tools==1.29.0 \
+    seldon-core==1.1.0 gitdb==4.0.5 protobuf==3.12.2
 pip install -r /projects/datascience-examples/workshop/src/train/requirements.txt
 
 echo "Running training"
