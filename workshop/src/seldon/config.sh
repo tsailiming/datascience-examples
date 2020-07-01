eb881c65c9f9459f7f216c9c555939fa6d411381
Tsai Li Ming <tsai.liming@gmail.com>
2020-07-01 23:39:41 +0800

    add config.sh

---
+++ workshop/src/seldon/config.sh
@@ -0,0 +1,4 @@
+# Change this to use the right model
+MODEL_NAME=LRModel # Without the .py
+IMAGE_NAME:lr  # Image name
+###
--- workshop/src/train/train-stage.sh
+++ workshop/src/train/train-stage.sh
@@ -3,8 +3,11 @@
 # Source code is checked ut at /workspace/source
 # Model should be written to /workspace/model
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. $DIR/config.sh
+
 pip install -U pip setuptools
 pip install -r /workspace/source/workshop/src/train/requirements.txt
 
 echo "Running training"
-python /workspace/source/workshop/src/train/lr.py -m /workspace/model -r lr
+python /workspace/source/workshop/src/train/$PYTHON_SCRIPT -m /workspace/model -r $RUN_NAME
